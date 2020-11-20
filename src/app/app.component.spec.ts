534f9ed9945f2628471b5a597d44f1774cf145c9
Stefano Sello <sellostefano@gmail.com>
2020-11-20 21:35:58 +0100

    Updated root build directory name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'easy-restaurant-frontend'`, () => {
+  it(`should have as title 'easy-restaurant-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('easy-restaurant-frontend');
+    expect(app.title).toEqual('easy-restaurant-client');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to easy-restaurant-frontend!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to easy-restaurant-client!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'easy-restaurant-frontend';
+  title = 'easy-restaurant-client';
 }
